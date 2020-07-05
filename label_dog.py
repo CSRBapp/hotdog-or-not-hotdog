1b567025c514a7f43e5fa5bfca7400ddbef0f5da
CSRBdev <dev@csrb.app>
2020-07-05 22:19:05 +0100

    [CSRB] Converted to a function and switched to OpenCV2.

--- label_dog.py
+++ label_dog.py
@@ -3,32 +3,37 @@ import sys
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-image = sys.argv[1]
-#^ Path to the image from command line
+def label_dog(image):
+    #^ Path to the image from command line
 
-image_file = tf.gfile.FastGFile(image, 'rb')
-#^ Image being read
+    image_file = tf.io.gfile.GFile(image, 'rb')
+    #^ Image being read
 
-data = image_file.read()
-#^ Data from image file
+    data = image_file.read()
+    #^ Data from image file
 
 
-# Loads label file, strips off carriage return
-classes = [line.rstrip() for line in tf.gfile.GFile("hot_dog_labels.txt")]
+    # Loads label file, strips off carriage return
+    #classes = [line.rstrip() for line in tf.io.gfile.GFile("hot_dog_labels.txt")]
+    classes = ["hotdog", "nothotdog"]
 
-# Unpersists graph from file
-with tf.gfile.FastGFile("hot_dog_graph.pb", 'rb') as inception_graph:
-    definition = tf.GraphDef()
-    definition.ParseFromString(inception_graph.read())
-    _ = tf.import_graph_def(definition, name='')
+    # Unpersists graph from file
+    with tf.io.gfile.GFile("hotdog-or-not-hotdog/hot_dog_graph.pb", 'rb') as inception_graph:
+        definition = tf.compat.v1.GraphDef()
+        definition.ParseFromString(inception_graph.read())
+        _ = tf.import_graph_def(definition, name='')
 
-with tf.Session() as session:
-    tensor = session.graph.get_tensor_by_name('final_result:0')
-    #^ Feeding data as input and find the first prediction
-    result = session.run(tensor, {'DecodeJpeg/contents:0': data})
-    
-    top_results = result[0].argsort()[-len(result[0]):][::-1] 
-    for type in top_results:
-        hot_dog_or_not = classes[type]
-        score = result[0][type]
-        print('%-20s : %.5f' % (hot_dog_or_not, score))
+    top_results = None
+
+    with tf.compat.v1.Session() as session:
+        tensor = session.graph.get_tensor_by_name('final_result:0')
+        #^ Feeding data as input and find the first prediction
+        result = session.run(tensor, {'DecodeJpeg/contents:0': data})
+
+        top_results = result[0].argsort()[-len(result[0]):][::-1]
+        for type in top_results:
+            hot_dog_or_not = classes[type]
+            score = result[0][type]
+            print('%-20s : %.5f' % (hot_dog_or_not, score))
+
+    return top_results
